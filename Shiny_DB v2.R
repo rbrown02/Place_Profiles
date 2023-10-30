d6ac7b8a3ab3c38ae004d8af0f87ab44ce41f49b
rbrown02 <128067285+rbrown02@users.noreply.github.com>
2023-10-30 10:11:34 +0000

    Update Shiny_DB v2.R

--- Shiny_DB v2.R
+++ Shiny_DB v2.R
@@ -715,7 +715,7 @@ server <- function(input, output) {
   
   year <- unique(pop_data$Timeperiod)
   
-  region_map <- read_excel("~/Place_Profile_Shiny_Dashboard/Mappings.xlsx")
+  region_map <- read_excel("~/Place_Profile_Shiny_Dashboard/Mappings.csv")
   
   pop_data_reg <- merge(x=pop_data, y=region_map, by="AreaName", all.x=TRUE)
   
